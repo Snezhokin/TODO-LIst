2ffe0ea59d84cfd88ee91d8e4a4d124b1f7af6b7
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-02-27 21:36:02 +0300

    add:  Empty list and Function: Add a new task to the list

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ task = {
     "title" : "Learn Python",
     "completed" : False
 }
+
+todo_list=[]
+
+def add_task(todo_list , title):
+    task = {
+        "id" : len(todo_list) + 1,
+        "title" : title,
+        "completed" : False
+    }
