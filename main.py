afb8628c26b77259e9bbc181bed94180c46d03a5
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-02-27 21:54:10 +0300

    Function: Changes tasks

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ def add_task(todo_list , title):
 
 add_task(todo_list , "Learn Python")
 print(todo_list)
+
+def update_task(todo_list, task_id , new_title):
+    for task in todo_list:
+        if task["id"] == task_id:
+            task["title"] = new_title
+            break
